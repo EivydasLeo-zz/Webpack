a3023b6a82d8e3d4f4c2cbc58d55f16317719df0
Your Name <you@example.com>
2021-05-20 10:35:29 +0300

    dev ir build scrips, index in dist

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,6 @@
-(()=>{"use strict";console.log("index.js"),console.log("add(2, 5, 2) =",9),console.log('say("nice day")',"i am saying "+"nice day")})();
+(() => {
+  "use strict";
+  console.log("index.js"),
+    console.log("add(2, 5, 2) =", 9),
+    console.log('say("nice day")', "i am saying " + "nice day");
+})();
