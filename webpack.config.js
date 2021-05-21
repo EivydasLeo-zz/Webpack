40e0c75e442ea4c7cfc02902d2b970061203575b
Your Name <you@example.com>
2021-05-21 09:29:57 +0300

    babel transpilling works

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  devtool: false, // kad kai sukompiliuojam development eitu geriau suprasti kada
   entry: {
     // nurodom musu programos pagrindini js faila
     main: path.resolve(__dirname, "./src/app.js"),
@@ -16,6 +17,16 @@ module.exports = {
       {
         test: /\.css$/i, // pritaikom taisykle tik *.css failams
         use: ["style-loader", "css-loader"], // perdarom .css failus i dist folderi
+      },
+      {
+        test: /\.js$/, // .js
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"], // kokia senumo js mes norim paversti veikianciu senesne narsyklese
+          },
+        },
       },
     ],
   },
