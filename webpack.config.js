eabf8ee9d33189b21fc4e6537e92218ad03d4a89
Your Name <you@example.com>
2021-05-21 10:40:42 +0300

    source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  devtool: false, // kad kai sukompiliuojam development eitu geriau suprasti kada
+  devtool: "source-map", // kad kai sukompiliuojam development eitu geriau suprasti kada
   devServer: {
     contentBase: "./dist",
   },
